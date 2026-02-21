65e7d289f2d569b674d663eb9d653ec16b290cfd
jandres3140-byte <jandres3140@gmail.com>
2026-02-21 04:23:41 -0300

    Update main.py

--- android/main.py
+++ android/main.py
@@ -1,1 +1,8 @@
-print("jcamp029.pro")
+from kivy.app import App
+from kivy.uix.label import Label
+
+class JCApp(App):
+    def build(self):
+        return Label(text="jcamp029.pro - APK funcionando ✅")
+
+JCApp().run()
